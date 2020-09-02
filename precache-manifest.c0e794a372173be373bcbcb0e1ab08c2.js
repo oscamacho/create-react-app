ce9f195a99ea710e875afadaa4344d66f19e6914
David <andrea.ascorti17@gmail.com>
2020-09-02 17:02:06 +0200

    Updates

---
+++ precache-manifest.c0e794a372173be373bcbcb0e1ab08c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98e2c816380d07668ef9a47ef94e7c3",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "3c5f299c970bb372c7a4",
+    "url": "/create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07509ff938fd588673d5",
+    "url": "/create-react-app/static/js/2.cb080641.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.cb080641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5f299c970bb372c7a4",
+    "url": "/create-react-app/static/js/main.dfca091c.chunk.js"
+  },
+  {
+    "revision": "6671d5cae3dcb49454f1",
+    "url": "/create-react-app/static/js/runtime-main.70d40b82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
