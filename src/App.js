1aaadcdd1d279be49b55c65fe03dc1d54422217a
David <andrea.ascorti17@gmail.com>
2020-09-03 23:45:54 +0200

    new version with routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
@@ -19,8 +26,56 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
     </div>
   );
 }
+
+const Home = () => 
+<>
+  <h1> home </h1>
+</>;
+
+const About = () => 
+  <div>
+    <h1>about pages</h1>
+  </div>;
+
+const User = () => 
+<div>
+  <h1>User</h1>
+</div>
 
 export default App;
