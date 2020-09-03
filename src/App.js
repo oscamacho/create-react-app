4070b9f22079c9b7758aeee81ba033161d73dc51
David <andrea.ascorti17@gmail.com>
2020-09-03 23:58:19 +0200

    hst

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloaaaaaad fucino.
+          Edit <code>src/App.js</code> and soventemente to reloaaaaaad fucino.
         </p>
         <a
           className="App-link"
@@ -23,7 +23,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactmybonn
         </a>
       </header>
 
